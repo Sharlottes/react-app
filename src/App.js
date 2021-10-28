ed7cf4d59520d08a9117349ddb16eb5522e0d69f
sharlottes <seeseafood@naver.com>
2021-10-28 12:35:53 +0900

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import Counter from './Counter';
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div onContextMenu={
+        (e) => {
+            e.preventDefault();
+        }
+    }>
         <Counter />
       </div>
     )
